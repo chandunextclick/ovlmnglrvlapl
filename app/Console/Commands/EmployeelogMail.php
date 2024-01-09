158c37104e239acb137d03e94455c739e53adb16
developer-edoxi <manageedoxi@gmail.com>
2024-01-09 15:08:46 +0530

    updated employeemailog for send indvidualmail

--- app/Console/Commands/EmployeelogMail.php
+++ app/Console/Commands/EmployeelogMail.php
@@ -33,9 +33,76 @@ class EmployeelogMail extends Command
     public function handle()
     {
 
+    
+
         $data['date1'] = date("Y-m-d", strtotime("-1 day"));
 
         $date1 = $data['date1'];
+
+        $users = DB::table('users')
+        ->where('company_id',4)
+        ->where('email', '!=', '')
+        ->get();
+
+    foreach ($users as $user) {
+
+        $query1 = "
+                    SELECT
+                    e1.empcode,
+                    users.name,
+                    e1.logdate,
+                    TIME_FORMAT(SEC_TO_TIME(SUM(
+                        CASE
+                            WHEN e1.direction = 'in'
+                            THEN TIMESTAMPDIFF(
+                                    SECOND,
+                                    STR_TO_DATE(CONCAT(e1.logdate, e1.logtime), '%Y-%m-%d %H:%i:%s'),
+                                    (SELECT MIN(STR_TO_DATE(CONCAT(e2.logdate, e2.logtime), '%Y-%m-%d %H:%i:%s'))
+                                    FROM employeelog e2
+                                    WHERE e2.empcode = e1.empcode
+                                    AND e2.logdate = e1.logdate
+                                    AND e2.logtime > e1.logtime
+                                    AND e2.direction = 'out')
+                            )
+                            ELSE 0
+                        END
+                    )), '%H:%i') AS total_working_time,
+                    TIME_FORMAT(SEC_TO_TIME(SUM(
+                        CASE
+                            WHEN e1.direction = 'out'
+                            THEN TIMESTAMPDIFF(
+                                    SECOND,
+                                    STR_TO_DATE(CONCAT(e1.logdate, e1.logtime), '%Y-%m-%d %H:%i:%s'),
+                                    (SELECT MIN(STR_TO_DATE(CONCAT(e2.logdate, e2.logtime), '%Y-%m-%d %H:%i:%s'))
+                                    FROM employeelog e2
+                                    WHERE e2.empcode = e1.empcode
+                                    AND e2.logdate = e1.logdate
+                                    AND e2.logtime > e1.logtime
+                                    AND e2.direction = 'in')
+                            )
+                            ELSE 0
+                        END
+                    )), '%H:%i') AS total_break_time
+                    FROM employeelog e1 
+                    LEFT JOIN employee_details ON employee_details.employee_id = e1.empcode
+                    LEFT JOIN users ON employee_details.user_id = users.id
+                    WHERE STR_TO_DATE(e1.logdate, '%Y-%m-%d') = '$date1' and users.id=$user->id
+                    GROUP BY e1.empcode, e1.logdate,users.name
+                    ";
+
+            
+
+            $data['essllog'] = DB::select($query1);
+
+            Mail::send('employees.ajax.mail',$data,function($messages) use ($user){
+
+            $messages->to($user->email);
+            $messages->subject('Log( '.date("Y-m-d", strtotime("-1 day")).' ) of '. $user->name);
+            
+            });
+
+        
+    }
 
         $query = "
         SELECT
@@ -84,14 +151,13 @@ class EmployeelogMail extends Command
     $data['query']=$query;
     $data['essllog'] = DB::select($query);
     
-    $user['name']='Next Click';
-    Mail::send('employees.ajax.mail',$data,function($messages) use ($user){
+    $useradmin['name']='Next Click';
+    Mail::send('employees.ajax.mail',$data,function($messages) use ($useradmin){
 
     $messages->to(['chandunextclick@gmail.com', 'neerajnextclick@gmail.com']);
     $messages->subject('Hello Admin');
 
 });
-
 
 
     }
