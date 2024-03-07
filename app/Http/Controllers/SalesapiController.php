2c4b1e88957d8288da37e8da3f7764250d1601f2
developer-edoxi <manageedoxi@gmail.com>
2024-03-07 11:15:17 +0530

    testing purpose

--- app/Http/Controllers/SalesapiController.php
+++ app/Http/Controllers/SalesapiController.php
@@ -16,9 +16,17 @@ class SalesapiController
     public function salestasksdata(Request $request){
 
 
-        var_dump("test");
+        $date = $request->input('date');
+        $tasktitle = $request->input('tasktitle');
+        $desc = $request->input('desc');
+        $deadline = $request->input('deadline');
+        $refid = $request->input('refid');        
 
-       
+        $result = DB::table('sales_tasks')->insert(['date' => $date,'tasktitle' => $tasktitle,'description' => $desc,'deadline' => $deadline,'refid' => $refid]);
+
+        var_dump($result);
+
+        return Reply::dataOnly(['status' => 'success','result'=>$result]);
     }
 
 }
